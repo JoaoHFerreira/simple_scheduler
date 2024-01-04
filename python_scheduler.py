a296ef1d1b5ce242df2a20508e05fbf6fd37f75c
JoaoHFerreira <jaycops@gmail.com>
2024-01-04 13:55:22 -0300

    1. create a simple method for running tasks from python 2, for now the job frquency is determined in th program according with the time frame used in time lib 3. For a next step some parameters should be offered by some kind of file to avoid over configurations on program, yaml for instance

--- python_scheduler.py
+++ python_scheduler.py
@@ -1,18 +1,17 @@
+
 import subprocess
 import os
+import time
 
-def run_program(language, file_path):
-    match language:
-        case "go":
-            subprocess.run([file_path])
-        case "python":
-            subprocess.run(["python", file_path])
+
+def run_scheduler():
+    subprocess.run(["python", f"{os.getcwd()}/python/hello.py"])
+
 
 if __name__ == "__main__":
-    programs = [
-        ("go", f"{os.getcwd()}/golang/hello"),
-        ("python", f"{os.getcwd()}/python/hello.py"),
-    ]
+    while True:
+        time.sleep(2)
+        run_scheduler()
+        print("running")
 
-    for language, file_path in programs:
-        run_program(language, file_path)
+
